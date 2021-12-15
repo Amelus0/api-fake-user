10d4db908b4ce2ba5f954e2970ec422b0a9d74a7
Amelus0 <ameluspaul2001@gmail.com>
2021-12-15 10:30:54 -0300

    Create function showProfileUser

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,19 @@
 fetch("https://randomuser.me/api/")
 .then((response) => response.json())
-.then((data) => {
-    console.log(data);
-});
+.then((data) => showProfileUser(data));
+
+function showProfileUser(data) {
+    const profile = data.results[0];
+    const thumbnail = profile.picture.large;
+    const name = `${profile.name.first} ${profile.name.last}`;
+    
+    const card = `<div class="card" style="width: 18rem;">
+    <img src="{thumbnail}" class="card-img-top" alt="avatar">
+    <div class="card-body">
+    <h5 class="card-title">History, My name is </h5>
+    <p class="card-text">${name}</p>
+    </div>
+    </div>`;
+
+    targetCards.innerHTML = card;
+}
